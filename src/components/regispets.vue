b35223a074a3b6635a3dec0648940f17bc079d00
Chaiyapat Sirisaksopa <chaiyapat69@gmail.com>
2017-06-23 22:54:45 +0700

    New register page

--- src/components/regispets.vue
+++ src/components/regispets.vue
@@ -1,15 +1,58 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div id="resgister">
+    <v-layout row>
+        <v-flex xs6 order-xs1>
+          Pic.
+          
+        </v-flex>
+        <v-flex xs6 order-xs2>
+          <input type="file" accept="image/*" capture="camera" id="imageUrl"  />
+            <img id="frame"><br/>
+        </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs6 order-xs1>
+          Detail 
+          
+        </v-flex>
+        <v-flex xs6 order-xs2>
+          <v-text-field
+              name="input-7-1"
+              multi-line
+              v-model="detail"
+              id="detail"
+            ></v-text-field>
+        </v-flex>
+
+
+      </v-layout>
+    <v-layout row>
+      <v-flex xs6 order-xs1>
+          Type 
+        </v-flex>
+        <v-flex xs6 order-xs2>
+          <v-select
+              v-bind:items="items"
+              v-model="selectType"
+              class="input-group--focused"
+              dark
+              item-value="text"
+            ></v-select>
+        </v-flex>
+
+
+      </v-layout>
+
+      <v-layout row>
+        <v-flex xs6 order-xs1></v-flex>
+        <v-flex xs6 order-xs2>
+          <v-btn primary light @click.native="submitform">Send</v-btn>
+          <v-btn dark default>Cannel</v-btn>
+        </v-flex>
+      </v-layout>
+  </div>
   
-  <!--<div class="row">
-    <div class="form-group row">
-    <label for="avatar" class="col-xs-5 align-right col-form-label">รูปโปรไฟล์</label>
-        <div class="col-xs-7">
-        <img src="//placehold.it/100" class="" alt="avatar">
-            <input  type="file" name="imageUrl" class="" id="imageUrl" accept="image/*" >
-        </div>
-    </div>
-  </div>-->
+  
 </template>
 
 <script>
